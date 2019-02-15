25a07fbc6d07070359ddd4d6e4bf9c3da051a66b
ASUSNotebook <adrianadyatma21@gmail.com>
2019-02-15 10:27:59 +0700

    finished mongo to sql

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import mongo_to_sql
 import credentials_var as cred
 
 if __name__ == '__main__':
+    # KEYWORD LENGTH LIMIT PER TEXT = 32
+
     keyword = ["Joko Widodo", "jokowi", "@jokowi"]
-    limit = 250
+    limit = 3
     stream_twitter.stream(keyword, limit)
 
-    keyword = ["Prabowo Subianto", "prabowo", "@jokowi"]
-    limit = 250
+    keyword = ["Prabowo Subianto", "prabowo", "@prabowo"]
+    limit = 3
     stream_twitter.stream(keyword, limit)
 
     mongo_to_sql.mongo_to_sql(cred.findall_tweets)
